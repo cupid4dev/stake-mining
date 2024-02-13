108eae194f0850b2ff0fce69326be8f723ee77e2
cupid4dev <davidlong.business526@gmail.com>
2024-02-12 21:05:34 -0800

    Initialized!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#1A2C38",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
